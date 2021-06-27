03303152e7454189d2cc1639265e9f5ffdbab5ac
Sang-Jin <lee3jjang@gmail.com>
2021-06-28 00:50:53 +0900

    dfdf

--- tests/test_main.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from myapp.main import read_root, read_item
-
-@pytest.mark.asyncio
-async def test_read_item():
-    result = await read_item(42)
-    assert result == {"item_id": 42, "q": None}
