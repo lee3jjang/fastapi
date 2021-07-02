c3bf0543ba8e83b633331a07f7822e852bf38c19
Sang-Jin <lee3jjang@gmail.com>
2021-07-02 22:42:56 +0900

    1212

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger('root')
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
-# from fastapi.templating import Jinja2Templates
-# templates = Jinja2Templates(directory='templates')
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory='templates')
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
 
-@app.get('/')
-def read_root():
-    return {"Hello": "World"}
+@app.get('/', response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"items_id": item_id, "q": q}
+    return {"items_id": item_id, "q": int(q)**2}
 
 @app.put('/items/{item_id}')
 def update_item(item_id: int, item: Item):
